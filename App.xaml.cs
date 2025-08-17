e8030635972a93b879f8203fc5af59a32b201322
AfterglowLionNel <lions7777@hotmail.com>
2025-08-17 15:32:24 +0900

    Initial commit for WPF application

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace analysCAR
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
